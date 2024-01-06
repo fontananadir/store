4f80ae9a47effac58f417c2379d275a1f0f1a4d4
Nadir Fontana <fontananadir@hotmail.it>
2024-01-06 21:11:03 +0100

    store

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,74 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: 'cart.component.html',
+})
+export class CartComponent implements OnInit {
+  cart: Cart = {items: [{
+    product: 'https://via.placeholder.com/150',
+    name: 'Dress',
+    price: 150,
+    quantity: 1,
+    id: 1,
+  },
+  {
+    product: 'https://via.placeholder.com/150',
+    name: 'Dress',
+    price: 150,
+    quantity: 3,
+    id: 2,
+  }]};
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action'
+  ]
+
+  constructor(private cartService: CartService, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
+  }
+
+  getTotal(items: Array<CartItem>): number{
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void{
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void{
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void{
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void{
+    this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void{
+    this.http.post('http://localhost:4242/checkout',
+    {items: this.cart.items}).subscribe(async(res: any) => {let stripe = await loadStripe('pk_test_51OVZZ0FKusGMGRhNfr6d4Np5sYPuGMlJqFMFNA0Fz027J6DfXSNl4bIKvjFBvXNkOB0gGsmFbfNNjbnZEacf5uyu00G7JK5p9g');
+    stripe?.redirectToCheckout({
+      sessionId: res.id
+      })
+    });
+  }
+}
---
+++ src/app/services/store.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../models/product.model';
+
+const STORE_BASE_URL = 'https://fakestoreapi.com';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  constructor(private HttpClient: HttpClient) { }
+
+  getAllProducts(limit= '12', sort= 'desc', category?: string): Observable<Array<Product>>{
+    return this.HttpClient.get<Array<Product>>(
+      `${STORE_BASE_URL}/products${category?'/category/'+category:''}?sort=${sort}&limit=${limit}`
+    )
+  }
+
+  getAllCategories(): Observable<Array<string>>{
+    return this.HttpClient.get<Array<string>>(
+      `${STORE_BASE_URL}/products/categories`
+    )
+  }
+}
